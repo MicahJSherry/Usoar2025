c63e1d0435021e4d0d331e2d18b5ce3817025396
Micah Sherry <MicahJSherry@gmail.com>
2026-02-25 17:09:34 -0500

    created slide show

--- fibb.py
+++ fibb.py
@@ -1,6 +1,6 @@
 from boolpoly import boolpoly
 
-k = 3
+k = 5
 xp1 = boolpoly(2**k+1)
 
 dict_f_n = {2**n-1: 0 for n in range(1,15)}
@@ -11,3 +11,17 @@ for i in range(2**16):
     if p in dict_f_n.keys():
         dict_f_n[p] += 1
 print([dict_f_n[p]for p in dict_f_n.keys()])    
+
+
+# conjecture
+
+fibb = [0, 1]
+for _ in range(12):
+    fibb.append(fibb[-1]+fibb[-2])
+print(fibb)
+N = 16
+for n in range(N+1):
+    q = n // k
+    r = n %  k
+    print((fibb[q+2]**r)*(fibb[q+1]**(k-r)))
+
